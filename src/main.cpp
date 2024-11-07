5ceaa86b517e9c06018ff7f214d7b33f25eb9c65
9erikSAntos6 <9xerikx6@gmail.com>
2024-11-07 18:35:31 -0300

    feat: Display LDC 16x2 mostra valores na tela

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
-#include "SensorTemperatura.h"
-#include "SensorTemperatura.h"
+#include <Wire.h>
+#include <BigCrystal.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(30, 31, 32, 33, 34, 35, 36, 37, 38, 39); 
+BigCrystal bigCrystal(&lcd);
+
+int temperaturaSimulada = 0;
+int humidadeSimulada = 50;
 
 void setup() {
-  Serial.begin(9600);
-}
+    bigCrystal.begin(16, 2);
+  }
 
 void loop() {
+  bigCrystal.print("Temperatura: ");bigCrystal.print(temperaturaSimulada);bigCrystal.print("oC");bigCrystal.setCursor(0, 1);
+  bigCrystal.print("Humidade: ");bigCrystal.print(humidadeSimulada);bigCrystal.print("%");
+
+  temperaturaSimulada += 1;
+  delay(2000);
+  bigCrystal.clear();
 }
