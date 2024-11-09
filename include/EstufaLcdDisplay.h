46b4a0326da6958192fa967d6e6156e8c0a39c74
9erikSantos6 <9xerikx6@gmail.com>
2024-11-09 16:43:48 -0300

    refact: Realiza abstração para uso do Display LCD

--- include/EstufaLcdDisplay.h
+++ include/EstufaLcdDisplay.h
@@ -7,8 +7,14 @@
 #include <BigCrystal.h>
 
 class EstufaLcdDisplay : public BigCrystal {
+  private:
+    LiquidCrystal lcd; // Objeto direto, não ponteiro
+
   public:
-    EstufaLcdDisplay(LiquidCrystal* lcd);
+    EstufaLcdDisplay(uint8_t rs, uint8_t enable,
+                 uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                 uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+
     void mostrarClimaEstufa(int temperaturaEstufa, int humidadeEstufa);
 };
 
--- src/modules/EstufaLcdDisplay/EstufaLcdDisplay.cpp
+++ src/modules/EstufaLcdDisplay/EstufaLcdDisplay.cpp
@@ -2,9 +2,17 @@
 #include <LiquidCrystal.h>
 #include "EstufaLcdDisplay.h"
 
-EstufaLcdDisplay::EstufaLcdDisplay(LiquidCrystal* lcd) : BigCrystal(lcd) {}
+EstufaLcdDisplay::EstufaLcdDisplay(uint8_t rs, uint8_t enable,
+                 uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                 uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) 
+    : lcd(rs, enable, d0, d1, d2, d3, d4, d5, d6, d7), BigCrystal(&lcd) {}
 
 void EstufaLcdDisplay::mostrarClimaEstufa(int temperaturaEstufa, int humidadeEstufa) {
-  this->print("Temperatura: ");this->print(temperaturaEstufa);this->print("oC");this->setCursor(0, 1);
-  this->print("Humidade: ");this->print(humidadeEstufa);this->print("%");
+  this->print("Temperatura: ");
+  this->print(temperaturaEstufa);
+  this->print("oC");
+  this->setCursor(0, 1);
+  this->print("Humidade: ");
+  this->print(humidadeEstufa);
+  this->print("%");
 }
